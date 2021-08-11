1242ac455acaf0aedbe106e8107aee36ccd0383b
zhaiyl07 <matkerimov.zhaiyl.ib9.1.19@gmail.com>
2021-08-11 21:35:20 +0600

    6 or 8 tasks done

--- script.js
+++ script.js
@@ -70,15 +70,85 @@
 // spinWords("This is a test") => "This is a test"
 // spinWords("This is another test") => "This is rehtona test"
 // ----------------------------------------------------
-function spinWords(string){
-    return string.split(" ").map(item => {
-        if (item.length > 5){
-            return item.split("").reverse().join("")
-        }
-        return item
-    }).join(" ")
-}
-console.log(spinWords("Hey fellow warriors"))
+// function spinWords(string){
+//     return string.split(" ").map(item => {
+//         if (item.length > 5){
+//             return item.split("").reverse().join("")
+//         }
+//         return item
+//     }).join(" ")
+// }
+// console.log(spinWords("Hey fellow warriors"))
+
+// task6 | Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who
+// have signed up to attend the next coding meetup that you are organising. The list is ordered according
+// to who signed up first.
+// Your task is to return one of the following strings:
+//     < firstName here >, < country here > of the first Python developer who has signed up; or
+//         There will be no Python developers if no Python developer has signed up.
+// ----------------------------------------------------
+// function getFirstPython(list) {
+//     let result = list.find(item => {
+//         return item.language === "Python"
+//     })
+//     return result ? `${result.firstName}, ${result.country} ` : "There will be no Python developers"
+// }
+// console.log(getFirstPython([
+//     { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+//     { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+//     { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+// ]))
+// console.log(getFirstPython([
+//     { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+//     { firstName: 'Amar', lastName: 'V.', country: 'Bosnia and Herzegovina', continent: 'Europe', age: 32, language: 'Ruby' },
+// ]))
+
+// task7 | Coding Meetup #7 - Higher-Order Functions Series - Find the most senior developer
+// You will be given a sequence of objects representing data about developers who have signed up to
+// attend the next coding meetup that you are organising.
+//     Your task is to return a sequence which includes the developer who is the oldest. In case of a tie,
+//     include all same-age senior developers listed in the same order as they appeared in the original input array.
+// ----------------------------------------------------
+// function findSenior(list) {
+//     let age = list.map((item ) => item.age)
+//     let mathAge = Math.max(...age)
+//     return list.filter(el => el.age === mathAge)
+// }
+// console.log(findSenior([
+//     { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+//     { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+//     { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+//     { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+// ]))
+
+// task8 | Find the unique number
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+//
+//     findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// ----------------------------------------------------
+// function findUniq(arr) {
+// return arr.find(item => {
+//     return arr.indexOf(item) === arr.lastIndexOf(item)
+// })
+// }
+// console.log(findUniq([ 1, 1, 1, 2, 1, 1 ]))
+// console.log(findUniq([ 0, 0, 0.55, 0, 0 ]))
+// ----------------------------------------------------
+
+// task9
+// ----------------------------------------------------
+// ----------------------------------------------------
+// ----------------------------------------------------
+// ----------------------------------------------------
+// ----------------------------------------------------
+// ----------------------------------------------------
+// ----------------------------------------------------
+// ----------------------------------------------------
+// ----------------------------------------------------
+// ----------------------------------------------------
+// ----------------------------------------------------
 // ----------------------------------------------------
 
 
