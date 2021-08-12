ddbc5e5fb4804bdbee22cd25a722ccd75e66d3ad
zhaiyl07 <matkerimov.zhaiyl.ib9.1.19@gmail.com>
2021-08-12 15:29:37 +0600

    for now only 1task

--- script.js
+++ script.js
@@ -159,8 +159,41 @@
 // }
 // console.log(persistence(9))
 // ----------------------------------------------------
+
+// task10 | Coding Meetup #10 - Higher-Order Functions Series - Create usernames
+// Given the following input array:
+//     var list1 = [
+//         { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby' },
+//         { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure' }
+//     ];
+// write a function that adds the username property to each object in the input array:
+//     The value of the username property is composed by concatenating:
+//
+//     firstName in lower-case;
+// first letter of the lastName in lower-case; and
+// the birth year which for the purpose of this kata is calculated simply by subtracting age from the current year.
+//     Please make sure that your function delivers the correct birth year irrespective of when it will be executed,
+// for example it should also work correctly for a meetup organised in 10-years-time. The example above assumes that the function is run in year 2020.
+// Notes:
+//     The input array will always be valid and formatted as in the example above.
+//     Age is represented by a number which can be any positive integer.
+//     Lastname will always be one upper-cased letter followed by dot, e.g. 'N.'
+// Order of the objects in the array should be maintained but order of the properties in the individual objects does not matter.
 // ----------------------------------------------------
+function addUsername(list) {
+    let year = new Date().getFullYear()
+    // let year = now.getFullYear()
+    return list.map(item => item.firstName.toLowerCase() + item.lastName[0].toLowerCase() + `${year - item.age}`  )
+}
+console.log(addUsername([
+    { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby' },
+    { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure' },
+    // {"firstName":"Harry","lastName":"K.","country":"Brazil","continent":"Americas","age":19,"language":"Python"}
+]))
 // ----------------------------------------------------
+// let now = new Date()
+// let year = now.getFullYear()
+// console.log( year -30)
 // ----------------------------------------------------
 // ----------------------------------------------------
 // ----------------------------------------------------
